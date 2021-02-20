339633f0989d13178746a924b888d746769c5b3a
Pooya Darvehei <pooya.darvehei@gmail.com>
2021-02-21 00:08:26 +0800

    added new features to location

--- solarlib/location.py
+++ solarlib/location.py
@@ -1,5 +1,5 @@
-from solarlib.exceptions import ValueOutOfRange,raise_type_error,type_check
-
+from solarlib.error_handling import ValueOutOfRange,raise_type_error,type_check
+from solarlib.solar import sunrise, sunset, estimated_irradiance
 class Location:
     def __init__(self, latitude, longitude, timezone=None):
         self.set_timezone(timezone)
@@ -54,10 +54,76 @@ class Location:
         lat = self.__latitude
         lon = self.__longitude
         tz = self.__timezone
-        output = f'''Location(latitude = {lat},
-            longitude = {lon},
+        output = f'''Location(latitude = {lat}, 
+            longitude = {lon}, 
             timezone = {str(tz)})'''
         return output.replace(' '*4,'').replace('\n','')
     
     def __str__(self):
         return repr(self)
+    
+    def sunrise(self,date,fmt = '%Y-%m-%d'):
+        date = self.__parse_date(date,fmt = '%Y-%m-%d')
+        lat = self.latitude
+        lon = self.longitude
+        base = sunrise(date,lat, lon)
+        return sunrise(base,lat, lon)
+        
+        
+    def sunset(self,date,fmt = '%Y-%m-%d'):
+        date = self.__parse_date(date,fmt = fmt)
+        lat = self.latitude
+        lon = self.longitude
+        base = sunset(date,lat, lon)
+        return sunset(base,lat, lon)
+    
+    def solar_irradiance(self,time,fmt = '%Y-%m-%d %H:%M:%S'):
+        time = self.__parse_time(time,fmt = fmt)
+        lat = self.latitude
+        lon = self.longitude
+        return estimated_irradiance(time,lat,lon)
+    
+    def daily_irradiance(self,date,fmt = '%Y-%m-%d',freq_min = 30):
+        date = self.__parse_date(date,fmt = fmt)
+        delta = datetime.timedelta(minutes = freq_min)
+        n = 1440//freq_min +1
+        output = [
+            (
+                date+i*delta,
+                self.solar_irradiance(date+i*delta)
+            ) for i in range(n)
+        ]
+        return output
+    
+    def day_length(self,date,fmt = '%Y-%m-%d'):
+        date = self.__parse_date(date,fmt = fmt)
+        rise_time = self.sunrise(date,fmt)
+        set_time = self.sunset(date,fmt)
+        return set_time-rise_time
+    
+    def solar_noon(self,date,fmt = '%Y-%m-%d'):
+        date = self.__parse_date(date,fmt = fmt)
+        lat = self.latitude
+        lon = self.longitude
+        base = solar_noon(date, lon)
+        return solar_noon(base, lon)
+
+    def __parse_date(self,date,fmt = '%Y-%m-%d'):
+        if isinstance(date,str):
+            return datetime.datetime.strptime(date,fmt)
+        elif isinstance(date,datetime.datetime):
+            return date
+        elif isinstance(date,datetime.date):
+            time = datetime.time()
+            return datetime.datetime.combine(date,time)
+        else:
+            raise_type_error('date',datetime.datetime)
+    
+    def __parse_time(self,date,fmt = '%Y-%m-%d %H:%M:%S'):
+        if isinstance(date,str):
+            return datetime.datetime.strptime(date,fmt)
+        elif isinstance(date,datetime.datetime):
+            return date
+        else:
+            raise_type_error('date',datetime.datetime)
+        
