9bfe6947ba2b176c0ef717c8677f61ec66ec4ea3
Pooya Darvehei <pooya.darvehei@gmail.com>
2021-02-21 00:15:31 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='solarlib',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A library for calculating solar irradiance, sunrise, sunset, and more. ',
+    author='Pooya Darvehei',
+    license='MIT',
+)
