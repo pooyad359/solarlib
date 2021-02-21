06baf3f2c02b49e4c6c5c9561db1c34c93eecceb
Pooya Darvehei <pooya.darvehei@gmail.com>
2021-02-21 15:41:48 +0800

    added download url to setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     description='A library for calculating solar irradiance, sunrise, sunset, and more. ',
     author='Pooya Darvehei',
     url='https://github.com/pooyad359/solarlib',
+    download_url='https://github.com/pooyad359/solarlib/archive/v0.1.0.tar.gz',
     license='MIT',
     install_requires=['pytz']
 )
